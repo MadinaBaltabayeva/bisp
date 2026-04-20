dfca4d93c7a33bef1c432ae3943b230d8b7fc5e1
MadinaBaltabayeva <madina@example.com>
2026-04-20 12:48:24 +0500

    fix sticky sidebar

--- src/components/browse/filter-sidebar.tsx
+++ src/components/browse/filter-sidebar.tsx
@@ -294,10 +294,8 @@ export function FilterSidebar(props: FilterSidebarProps) {
 
   return (
     <>
-      <aside className="hidden w-56 shrink-0 lg:block">
-        <div className="sticky top-20 max-h-[calc(100vh-6rem)] overflow-y-auto pr-2">
-          <FilterContent {...props} />
-        </div>
+      <aside className="hidden w-56 shrink-0 self-start sticky top-20 max-h-[calc(100vh-6rem)] overflow-y-auto pr-2 lg:block">
+        <FilterContent {...props} />
       </aside>
 
       <div className="lg:hidden">
